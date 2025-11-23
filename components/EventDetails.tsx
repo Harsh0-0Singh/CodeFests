6f01d0f38dcdb8ab4920a05f7ffdf656aeb65a5b
Harsh0-0Singh <harshsingh9hrs@gmail.com>
2025-11-23 15:35:38 +0530

    minor tailwind error fix

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -158,7 +158,7 @@ const EventDetails = async({params}:{params:Promise<string>}) => {
         </aside>
       </div>
 
-      <div className="flex w-full fleex-col gap-4 pt-20">
+      <div className="flex w-full flex-col gap-4 pt-20">
         <h2>Similar Events</h2>
         <div className="events">
           {similarEvents.length > 0 &&
